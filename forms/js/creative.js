a19c34b7c6e4b51546f7849bfbe26eb04d026c12
Josh Nye <josh@smrtsystems.com>
2016-07-19 14:37:25 -0700

    Initialize repo for nycvalets

---
+++ forms/js/creative.js
@@ -0,0 +1,95 @@
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
+
+
+    $('.noWidows2').each(function(i,d){ 
+    var value="&nbsp;";
+    $(d).html($(d).text().replace(/\s(?=[^\s]*$)/g, value).replace(/\s(?=[^\s]*$)/g, value)); 
+    });
+ 
+
+})(jQuery); // End of use strict
+
+$(document).ready(function(){
+
+    var form = $('#container_form');
+    form.load('message_form.html', '#contact_form');
+
+
+    $('#maid_service').click(function(){
+        form.load('/../ajax/maid_service.html');
+    });
+
+    $('#aptmnt_sitting').click(function(){
+        form.load('/../ajax/aptmnt_sitting.html');
+    });
+
+    $('#package_delivery').click(function(){
+        form.load('/../ajax/maid_service.html');
+    });
+
+    $('#pet_service').click(function(){
+        form.load('/../ajax/aptmnt_sitting.html');
+    });
+
+    $('#upholstery_cleaning').click(function(){
+        form.load('/../ajax/aptmnt_sitting.html');
+    });
+
+    $('#errands').click(function(){
+        form.load('/../ajax/maid_service.html');
+    });
+
+    $('#rug_cleaning').click(function(){
+        form.load('/../ajax/aptmnt_sitting.html');
+    });
+
+
+
+
+
+});
